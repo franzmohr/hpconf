47073a60f7ef839d0bc86e584faa8baf952e7dae
Franz X. Mohr <franz.x.mohr@outlook.com>
2018-11-04 11:14:26 +0100

    Redefine ci argument

--- R/hpconf.R
+++ R/hpconf.R
@@ -2,10 +2,10 @@
 #'
 #' Constructs confidence bands for the Hodrick-Prescott filter
 #'
-#' @param data an object of class "mFilter" containing the output of the function \code{hpfilter}.
+#' @param data an object of class "mFilter" containing the output of the function \code{\link[mFilter]{hpfilter}}.
 #' @param V_y a numeric specifying the variance of the cyclical component. If \code{V_y = NULL} (default),
-#' the sample variance of the original series will be used.
-#' @param ci numeric between 0 and 1 specifying the confidence interval, defaults to 0.05.
+#' the sample variance of the original time series will be used.
+#' @param ci numeric between 0 and 1 specifying the confidence interval, defaults to 0.95.
 #' 
 #' @details The function uses the filter matrix \eqn{F} from an "mFilter" object to obtain the matrix \eqn{Q = \left[I_T + \lambda K' K]^{-1}\right]}.
 #' Since \eqn{Q y} provides an estimate of the trend \eqn{\hat{tau}} and \eqn{F y} yields the cyclical component of the process \eqn{\hat{c}} and the
@@ -14,7 +14,7 @@
 #' 
 #' The confidence band is than derived from the covariance matrix \eqn{V(\hat{tau}) = Q V(y) Q}.
 #' 
-#' By default \code{V_y = NULL} so that the sample variance of the cyclical componenten of the time series
+#' By default \code{V_y = NULL} so that the sample variance of the original time series
 #' is used for the construction of \eqn{V(y)}. This can be chanced by providing
 #' a numeric value, for example from the output of an esitmated ARIMA model for the cyclical component of the series.
 #' 
@@ -31,7 +31,7 @@
 #' Giles, D. E. (2013). Constructing confidence bands for the Hodrick-Prescott filter. \emph{Applied Economics Letters, 20}(5), 480--484. \url{https://doi.org/10.1080/13504851.2012.714057}.
 #'
 #' @export
-hpconf <- function(data, V_y = NULL, ci = .05) {
+hpconf <- function(data, V_y = NULL, ci = .95) {
   if(!requireNamespace("mFilter")){
     stop("This packages requires the 'mFilter' package.")
   }
@@ -59,6 +59,7 @@ hpconf <- function(data, V_y = NULL, ci = .05) {
 
   V_t <- Q %*% V_y %*% Q
   se <- matrix(sqrt(diag(V_t)))
+  ci <- 1 - ci
   ci <- stats::qnorm(1 - ci / 2)
   ci_low <- trend - ci * se
   ci_high <- trend + ci * se
