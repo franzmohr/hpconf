17be8a53813b9a4d86599167589727f20005b19e
Franz X. Mohr <franz.x.mohr@outlook.com>
2018-11-06 22:43:45 +0100

    New commit

--- R/hpconf.R
+++ R/hpconf.R
@@ -1,22 +1,22 @@
 #' Confidence Bands for the Hodrick-Prescott Filter
 #'
-#' Constructs confidence bands for the Hodrick-Prescott filter
+#' A function for the calculation of confidence bands for the trend component of the Hodrick-Prescott (HP) filter as proposed by Giles (2013).
 #'
 #' @param data an object of class "mFilter" containing the output of the function \code{\link[mFilter]{hpfilter}}.
 #' @param V_y a numeric specifying the variance of the cyclical component. If \code{V_y = NULL} (default),
 #' the sample variance of the original time series will be used.
-#' @param ci numeric between 0 and 1 specifying the confidence interval, defaults to 0.95.
+#' @param ci numeric between 0 and 1 specifying the confidence interval. Defaults to 0.95.
 #' 
 #' @details The function uses the filter matrix \eqn{F} from an "mFilter" object to obtain the matrix \eqn{Q = \left[I_T + \lambda K' K]^{-1}\right]}.
 #' Since \eqn{Q y} provides an estimate of the trend \eqn{\hat{tau}} and \eqn{F y} yields the cyclical component of the process \eqn{\hat{c}} and the
 #' time series \eqn{y} is decomposed only into those two components so that \eqn{y = \hat{tau} + \hat{c}},
 #' \eqn{Q} can be obtained from \eqn{I_T y - Fy = Q y} so that \eqn{Q = I_T - F}.
 #' 
-#' The confidence band is than derived from the covariance matrix \eqn{V(\hat{tau}) = Q V(y) Q}.
+#' The confidence band is then derived from the covariance matrix \eqn{V(\hat{tau}) = Q V(y) Q}.
 #' 
 #' By default \code{V_y = NULL} so that the sample variance of the original time series
 #' is used for the construction of \eqn{V(y)}. This can be chanced by providing
-#' a numeric value, for example from the output of an esitmated ARIMA model for the cyclical component of the series.
+#' a numeric value, for example from the output of an esitmated ARIMA model.
 #' 
 #' @return A time-series object of four variables
 #' \itemize{
